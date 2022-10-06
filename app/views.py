e20673bf4c5f1c2aa8dacc84bea39c78d76e6bbf
monja199 <monja.sesame@gmail.com>
2022-10-06 16:12:40 +0300

    updating

--- app/views.py
+++ app/views.py
@@ -5,25 +5,15 @@ from django.http import request
 from app.forms import NewUserForm, Authentificaton
 from django.contrib.auth.hashers import make_password, check_password
 
-def auth(request):
-    msg = 'ok '
-    return render(request, 'base.html', locals())
-
 
 # forms view
 def new_user(request):
     form = NewUserForm(request.POST)
-    boolean = False
     if form.is_valid():
-        boolean = True
-        msg = 'register ok'
-        error_msg = ''
+        msg, error_msg = 'register ok', ''
 
-        # mail
-        mail = form.cleaned_data['mail']
-        # pass
-        password = form.cleaned_data['password']
-        repeate = form.cleaned_data['repeate']
+        # auth
+        mail, password, repeate = form.cleaned_data['mail'], form.cleaned_data['password'], form.cleaned_data['repeate']
 
         # verification si les deux mots de passe sont les même
         if password != repeate:
@@ -37,8 +27,7 @@ def new_user(request):
                 error_msg = 'Email déjà utilisé par un autre utilisateur'
             except User.DoesNotExist:
                 # about
-                user.first_name = form.cleaned_data['first_name']
-                user.last_name = form.cleaned_data['last_name']
+                user.first_name, user.last_name = form.cleaned_data['first_name'], form.cleaned_data['last_name']
                 user.gender = form.cleaned_data['gender']
 
                 # company
@@ -67,11 +56,41 @@ def auth(request):
     auth = Authentificaton(request.POST)
     bolean = False
     if auth.is_valid():
-        msg = 'validate'
-        bolean = True
-        return HttpResponse('ok valid')
+        msg = ''
+
+        # authentification value
+        mail = auth.cleaned_data['Email']
+        password = auth.cleaned_data['Password']
+        try:
+            # mail exists ?
+            user = User.objects.get(mail=mail)
+
+            # password True ?
+            if check_password(password, user.password):
+                # valid
+                request.session['id'] = user.id
+                return redirect(home)
+            else:
+                msg = 'Wrong password'
+                return render(request, 'authentification/auth.html', locals())
+
+        except User.DoesNotExist:
+            msg = 'Identifiant inconnu'
+
+            return render(request, 'authentification/auth.html', locals())
 
     else:
         auth = Authentificaton()
 
         return render(request, 'authentification/auth.html', locals())
+
+def home(request):
+    text = 'Welcome'
+
+    try:
+        session = request.session['id']
+        msg = 'ok'
+        return render(request, 'tab/home.html', locals())
+    except KeyError:
+        return redirect(auth)
+
--- crm/urls.py
+++ crm/urls.py
@@ -14,12 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', app.views.auth),
-    path('register/', app.views.new_user, name='register'),
-    path('auth/', app.views.auth, name='authentification'),
+    re_path('^auth/$', app.views.auth),
+
+    # form
+    re_path('^register/$', app.views.new_user, name='register'),
+    re_path('^auth/$', app.views.auth, name='authentification'),
+
+    # tab
+    re_path('^home/$', app.views.home, name='home'),
 ]
